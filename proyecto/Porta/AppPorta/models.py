6b0ab40d36cf5d2cc01b6c2c04aad33b1951321b
inaserrano <kitoserranokito@gmail.com>
2023-06-02 00:50:33 -0300

    Añadi una fecha_create y otra fecha_update

--- proyecto/Porta/AppPorta/models.py
+++ proyecto/Porta/AppPorta/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
     nombre = models.CharField(max_length=20,default="Python")
-    camada = models.IntegerField()
-    
-    
+    camada = models.IntegerField(default=10000)
+    fecha_create = models.DateTimeField(auto_created=True,null=True)
+    fecha_update = models.DateTimeField(auto_now=True,null=True)
